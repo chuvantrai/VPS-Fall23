7c5beff88d4de5d55ce7d1ccab5da8c350ef61de
khoatnd <khoatndhe153511@fpt.edu.vn>
2023-10-04 20:46:32 +0700

    add mobile app

--- VPS-Fall23/Client.MobileApp/MauiProgram.cs
+++ VPS-Fall23/Client.MobileApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace Client.MobileApp
+﻿using Microsoft.Extensions.Logging;
+
+namespace Client.MobileApp
 {
     public static class MauiProgram
     {
@@ -12,6 +14,10 @@
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
 
             return builder.Build();
         }
