5a78d255a3b9f8293fa2ede5ce7565fc278bf79a
nhiep <nhiep2371@gmail.com>
2023-10-06 17:10:26 +0700

    add generic repository

---
+++ VPS-Fall23/Service.ManagerVPS/Repositories/Interfaces/Base/IVpsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Service.ManagerVPS.Repositories.Interfaces.Base
+{
+    public interface IVpsRepository<TEntity>
+        where TEntity : class
+    {
+        DbSet<TEntity> Entities { get; }
+        Task<TEntity> Create(TEntity entity);
+        Task Delete(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task<TEntity> Find(params object[][] keys);
+        Task<int> SaveChange();
+    }
+}
