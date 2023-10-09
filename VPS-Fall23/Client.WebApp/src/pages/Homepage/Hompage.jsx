56761050ad2bb8d3d10eb29c4d36726f94c1019e
TinhLD <59137707+TinhLeDanh@users.noreply.github.com>
2023-10-09 22:16:02 +0700

    update homepage

--- VPS-Fall23/Client.WebApp/src/pages/Homepage/Hompage.jsx
+++ VPS-Fall23/Client.WebApp/src/pages/Homepage/Hompage.jsx
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import style from './Homepage.module.scss'
+import { Input, Button } from 'antd';
+
+const cx = classNames.bind(style);
+
 function Homepage() {
-  return <h2>Home Page</h2>;
+  return <div className={cx("home-container w-full h-full m-auto bg-slate-500 block")}>
+    <Input className={cx('search-input')} placeholder="Search parking zone" />
+    <Button className={cx('btn')}>Find near parking zone</Button>
+  </div>;
 }
 
 export default Homepage;
