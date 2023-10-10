f34833f1c9739277dca16b064c8ada9ec7a38f9e
quangthai <quangthai05122001@gmail.com>
2023-10-10 23:43:42 +0700

    [feat]: update register new parking zone

--- VPS-Fall23/Service.ManagerVPS/Controllers/ParkingZoneController.cs
+++ VPS-Fall23/Service.ManagerVPS/Controllers/ParkingZoneController.cs
@@ -3,6 +3,7 @@ using Service.ManagerVPS.Constants.Notifications;
 using Service.ManagerVPS.Controllers.Base;
 using Service.ManagerVPS.DTO.Exceptions;
 using Service.ManagerVPS.DTO.Input;
+using Service.ManagerVPS.ExternalClients;
 using Service.ManagerVPS.Models;
 using Service.ManagerVPS.Repositories.Interfaces;
 
@@ -10,9 +11,13 @@ namespace Service.ManagerVPS.Controllers;
 
 public class ParkingZoneController : VpsController<ParkingZone>
 {
-    public ParkingZoneController(IParkingZoneRepository parkingZoneRepository)
+    private readonly IConfiguration _config;
+
+    public ParkingZoneController(IParkingZoneRepository parkingZoneRepository,
+        IConfiguration config)
         : base(parkingZoneRepository)
     {
+        _config = config;
     }
 
     [HttpPost]
@@ -30,6 +35,21 @@ public class ParkingZoneController : VpsController<ParkingZone>
             PricePerHour = input.PricePerHour,
             PriceOverTimePerHour = input.PriceOverTimePerHour
         };
+
+        var fileManager =
+            new FileManagementClient(_config.GetValue<string>("fileManagementAccessKey:baseUrl"),
+                _config.GetValue<string>("fileManagementAccessKey:accessKey"),
+                _config.GetValue<string>("fileManagementAccessKey:secretKey"));
+        var parkingZoneImgs = new MultipartFormDataContent();
+        for (var i = 0; i < input.ParkingZoneImages.Count; i++)
+        {
+            var streamContent = new StreamContent(input.ParkingZoneImages[i].OpenReadStream());
+            parkingZoneImgs.Add(streamContent, FileManagementClient.MULTIPART_FORM_PARAM_NAME,
+                $"{newParkingZone.Id}-{i}.{Path.GetExtension(input.ParkingZoneImages[i].FileName)}");
+        }
+
+        await fileManager.Upload("nghianv-vps-public",
+            $"parking-zone-images/{input.OwnerId}/{newParkingZone.Id}", parkingZoneImgs);
 
         var registerParkingZoneResult =
             await ((IParkingZoneRepository)vpsRepository).Create(newParkingZone);
