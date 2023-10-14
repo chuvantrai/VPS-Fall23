94d656929c38bf785135b0c628d35255968f3dcc
TinhLD <59137707+TinhLeDanh@users.noreply.github.com>
2023-10-15 01:54:57 +0700

    ignore

--- VPS-Fall23/Client.WebApp/src/layouts/components/Content/ContentLayout.jsx
+++ VPS-Fall23/Client.WebApp/src/layouts/components/Content/ContentLayout.jsx
@@ -1,14 +1,10 @@
 import { Layout, Breadcrumb, theme } from 'antd';
 import PropTypes from 'prop-types';
-
-import UserProfile from '@/pages/Homepage/components/Content/UserProfile';
-import ViewListParkingZone from '@/pages/Homepage/components/Content/ViewListParkingZone';
-import RegisterParkingZone from '@/pages/RegisterParkingZone/RegisterParkingZone';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const { Content } = Layout;
 
-function ContentLayout({ contentState, title, desc }) {
+function ContentLayout({ title, desc }) {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
@@ -16,11 +12,10 @@ function ContentLayout({ contentState, title, desc }) {
   return (
     <Layout className="bg-[#f0f2f5]">
       <div className="w-full bg-white py-[16px] px-[24px] mb-[20px]">
-        {/* <Breadcrumb>
+        <Breadcrumb>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb> */}
+        </Breadcrumb>
         <div className="w-[808px] h-11 justify-start items-center gap-4 inline-flex mt-[8px] mb-[8px]">
           <div className="justify-start items-center gap-3 flex">
             <div className="text-black text-opacity-90 text-[22px] font-medium font-['Roboto'] leading-7">{title}</div>
@@ -43,9 +38,6 @@ function ContentLayout({ contentState, title, desc }) {
           }}
         >
           <Outlet></Outlet>
-          {/* {contentState === '1' && <UserProfile></UserProfile>}
-          {contentState === '2' && <ViewListParkingZone></ViewListParkingZone>}
-          {contentState === '3' && <RegisterParkingZone />} */}
         </Content>
       </div>
     </Layout>
