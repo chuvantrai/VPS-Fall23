9844dd4e208fe45c183ce04ea56a6f6b53d4f03e
traicv <traicv@nvoaon.asia>
2023-10-18 09:52:38 +0700

    chá»ore: create api test vnpay

--- VPS-Fall23/Service.ManagerVPS/Controllers/TestApiController.cs
+++ VPS-Fall23/Service.ManagerVPS/Controllers/TestApiController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Service.ManagerVPS.Constants.Enums;
 using Service.ManagerVPS.DTO.Input;
 using Service.ManagerVPS.Repositories.Interfaces;
 using Service.ManagerVPS.Controllers.Base;
 using Service.ManagerVPS.DTO.OtherModels;
 using Service.ManagerVPS.Extensions.ILogic;
+using Service.ManagerVPS.Extensions.Logic;
 using Service.ManagerVPS.FilterPermissions;
 using Service.ManagerVPS.Models;
 
@@ -68,20 +70,77 @@ public class TestApiController : VpsController<Account>
         _vnPayLibrary.AddRequestData("vnp_CurrCode", "VND");
         _vnPayLibrary.AddRequestData("vnp_IpAddr", GetIpAddress() ?? "123456789");
         _vnPayLibrary.AddRequestData("vnp_Locale", "vn");
-        _vnPayLibrary.AddRequestData("vnp_OrderInfo", "Thanh toan don hang:" + order.OrderId);
+        _vnPayLibrary.AddRequestData("vnp_OrderInfo", "Thanh toan don hang: " + order.OrderId);
         _vnPayLibrary.AddRequestData("vnp_OrderType", "other"); //default value: other
         _vnPayLibrary.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
         _vnPayLibrary.AddRequestData("vnp_TxnRef", order.OrderId.ToString());
         var paymentUrl = _vnPayLibrary.CreateRequestUrl(vnp_Url, vnp_HashSecret);
+        var orderId = order.OrderId; // mã giao dịch
         return Ok(new
         {
             PaymentUrl = paymentUrl,
-            OrderId = order.OrderId, // id giao dịch
-            VnpTmnCode = vnp_TmnCode
+            vnp_TxnRef = orderId.ToString(),
+            vnp_CreateDate = order.CreatedDate.ToString("yyyyMMddHHmmss")
         });
     }
 
-    private string? GetIpAddress()
+    [HttpPost]
+    public IActionResult QueryDrVnPayDemo(string vnpTxnRef, string vnpCreateDate)
+    {
+        var vnp_Api = _config.GetValue<string>("vnpayConfig:vnp_Api");
+        var vnp_HashSecret = _config.GetValue<string>("vnpayConfig:vnp_HashSecret"); //Secret KEy
+        var vnp_TmnCode = _config.GetValue<string>("vnpayConfig:vnp_TmnCode"); // Terminal Id
+
+        //Mã hệ thống merchant tự sinh ứng với mỗi yêu cầu truy vấn giao dịch.
+        //Mã này là duy nhất dùng để phân biệt các yêu cầu truy vấn giao dịch. Không được trùng lặp trong ngày.
+        var vnp_RequestId = DateTime.Now.Ticks.ToString(); 
+        var vnp_Version = _vnPayLibrary.GetVersionVnPay(); //2.1.0
+        var vnp_Command = "querydr";
+        var vnp_TxnRef = vnpTxnRef; // Mã giao dịch thanh toán tham chiếu
+        var vnp_OrderInfo = "Truy van giao dich:" + vnpTxnRef;
+        var vnp_TransactionDate = vnpCreateDate;
+        var vnp_CreateDate = DateTime.Now.ToString("yyyyMMddHHmmss");
+        var vnp_IpAddr = GetIpAddress();
+
+        var signData = vnp_RequestId + "|" + vnp_Version + "|" + vnp_Command + "|" + vnp_TmnCode + "|" + vnp_TxnRef +
+                       "|" + vnp_TransactionDate + "|" + vnp_CreateDate + "|" + vnp_IpAddr + "|" + vnp_OrderInfo;
+        var vnp_SecureHash = Utils.HmacSHA512(vnp_HashSecret, signData);
+
+        var qdrData = new
+        {
+            vnp_RequestId = vnp_RequestId,
+            vnp_Version = vnp_Version,
+            vnp_Command = vnp_Command,
+            vnp_TmnCode = vnp_TmnCode,
+            vnp_TxnRef = vnp_TxnRef,
+            vnp_OrderInfo = vnp_OrderInfo,
+            vnp_TransactionDate = vnp_TransactionDate,
+            vnp_CreateDate = vnp_CreateDate,
+            vnp_IpAddr = vnp_IpAddr,
+            vnp_SecureHash = vnp_SecureHash
+        };
+        var jsonData = System.Text.Json.JsonSerializer.Serialize(qdrData);
+
+        var httpWebRequest = (HttpWebRequest)WebRequest.Create(vnp_Api);
+        httpWebRequest.ContentType = "application/json";
+        httpWebRequest.Method = "POST";
+
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        {
+            streamWriter.Write(jsonData);
+        }
+
+        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        var strData = "";
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            strData = streamReader.ReadToEnd();
+        }
+
+        return Ok(strData);
+    }
+
+    private string GetIpAddress()
     {
         string? ipAddress;
         try
@@ -97,6 +156,6 @@ public class TestApiController : VpsController<Account>
             ipAddress = "Invalid IP:" + ex.Message;
         }
 
-        return ipAddress;
+        return ipAddress??"::1";
     }
 }
