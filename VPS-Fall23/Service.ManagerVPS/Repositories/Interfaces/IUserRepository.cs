10411c9760891ce793215f1c5f32119535354053
quangthai <quangthai05122001@gmail.com>
2023-11-01 23:46:48 +0700

    [feat] view list attendant and searching

--- VPS-Fall23/Service.ManagerVPS/Repositories/Interfaces/IUserRepository.cs
+++ VPS-Fall23/Service.ManagerVPS/Repositories/Interfaces/IUserRepository.cs
@@ -1,5 +1,5 @@
-
 using Service.ManagerVPS.DTO.Input;
+using Service.ManagerVPS.DTO.OtherModels;
 using Service.ManagerVPS.Models;
 
 namespace Service.ManagerVPS.Repositories.Interfaces;
@@ -9,22 +9,27 @@ public interface IUserRepository : IVpsRepository<Account>
     bool CheckEmailExists(string email);
 
     bool CheckValidVerification(string email, int code);
-    
+
     string AddUser();
-    
+
     Account? GetAccountByEmail(string email);
-    
+
     Account? GetOwnerAccountByEmail(string email);
 
     void VerifyAccount(Account account);
-    
+
     Task<Account?> GetAccountByUserNameAsync(string userName);
 
     Task<Account?> UpdateVerifyCodeAsync(string userName, string? emailSubject = null);
-    
+
     Task<Account?> ChangePasswordByUserIdAsync(Guid id, string password);
-    
+
     Task<Account?> UpdateAccountById(UpdateProfileAccountRequest request);
-    
+
     Task<Account?> GetAccountByIdAsync(Guid id);
+
+    PagedList<Account> GetListAttendantAccount(string ownerId, QueryStringParameters parameters);
+
+    PagedList<Account> SearchAttendantByName(string ownerId, string attendantName,
+        QueryStringParameters parameters);
 }
