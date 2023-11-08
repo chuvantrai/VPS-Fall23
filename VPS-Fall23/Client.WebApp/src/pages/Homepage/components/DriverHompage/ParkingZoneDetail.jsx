9c9e8428f32ff3fc2be127285d4642b3831fbbf0
Chu Van Trai (k15 HL) <traicvhe153014@fpt.edu.vn>
2023-11-09 01:20:00 +0700

    chore: update bookmark

--- VPS-Fall23/Client.WebApp/src/pages/Homepage/components/DriverHompage/ParkingZoneDetail.jsx
+++ VPS-Fall23/Client.WebApp/src/pages/Homepage/components/DriverHompage/ParkingZoneDetail.jsx
@@ -1,165 +1,178 @@
-import { Button, Carousel, Descriptions, Image, Modal, Tabs, Tag, Typography, notification } from 'antd';
+import {Button, Carousel, Descriptions, Image, Modal, Tabs, Tag, Typography, notification, Tooltip} from 'antd';
 import useParkingZoneService from '../../../../services/parkingZoneService';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import BookingForm from './BookingForm';
 import FeedBackForm from '@/pages/Homepage/components/DriverHompage/FeedBackForm.jsx';
 import FeedbackList from './FeedbackList';
-import { BookOutlined, BookTwoTone } from '@ant-design/icons';
-import { getListBookmarkParkingZone, setListBookmarkParkingZone } from '../../../../helpers/index.js';
+import {BookOutlined, BookTwoTone} from '@ant-design/icons';
+import {getListBookmarkParkingZone, setListBookmarkParkingZone} from '../../../../helpers/index.js';
 
 
-const ParkingZoneDetail = ({ parkingZone, isShow, onCloseCallback, defaultTab = '1' }) => {
+const ParkingZoneDetail = ({parkingZone, isShow, onCloseCallback, defaultTab = '1'}) => {
 
-  const parkingZoneService = useParkingZoneService();
-  const [imageLinks, setImageLinks] = useState([]);
-  const [freeSlots, setFreeSlots] = useState(parkingZone?.slots ?? 0);
-  const [tab, setTab] = useState('1');
-  useEffect(() => {
-    setTab(defaultTab);
-  }, [defaultTab]);
-  useEffect(() => {
+    const parkingZoneService = useParkingZoneService();
+    const [imageLinks, setImageLinks] = useState([]);
+    const [freeSlots, setFreeSlots] = useState(parkingZone?.slots ?? 0);
+    const [tab, setTab] = useState('1');
+    useEffect(() => {
+        setTab(defaultTab);
+    }, [defaultTab]);
+    useEffect(() => {
+        if (isShow && parkingZone !== undefined && parkingZone.id) {
+            const arrayBookmarkPzId = getListBookmarkParkingZone() ?? [];
+            setIsBookmark(arrayBookmarkPzId.includes(parkingZone?.id));
+        }
+        if (!parkingZone) return;
+        parkingZoneService.getImageLink(parkingZone.id).then((res) => setImageLinks(res?.data));
+    }, [parkingZone?.id]);
+    const onGetFreeSlot = (parkingZoneId) => {
+        parkingZoneService.getBookedSlot(parkingZoneId).then(res => setFreeSlots(parkingZone.slots - res.data));
+    };
+    const getDetailDescription = () => {
+        if (!parkingZone) return [];
+        return [
+            {
+                key: 1,
+                label: 'Địa chỉ',
+                children: parkingZone.detailAddress,
+            },
+            {
+                key: 2,
+                label: 'Xã/Phường',
+                children: parkingZone.commune.name,
+            },
+            {
+                key: 3,
+                label: 'Quận/Huyện',
+                children: parkingZone.commune.district.name,
+            },
+            {
+                key: 4,
+                label: 'Tỉnh/Thành phố',
+                children: parkingZone.commune.district.city.name,
+            },
+            {
+                key: 5,
+                label: 'Giá thành mỗi giờ (VNĐ)',
+                children: parkingZone.pricePerHour ?? 0,
+            },
+            {
+                key: 6,
+                label: 'Giá khi đỗ quá giờ (VNĐ)',
+                children: parkingZone.priceOverTimePerHour ?? 0,
+            },
+            {
+                key: 5,
+                label: 'Số chỗ trống',
+                children: (
+                    <Button
+                        onClick={() => onGetFreeSlot(parkingZone.id)}
+                    >{freeSlots ?? parkingZone.slots}</Button>
+                ),
+            },
+        ];
+    };
+    const items = [
+        {
 
-    if (!parkingZone) return;
-    parkingZoneService.getImageLink(parkingZone.id).then((res) => setImageLinks(res?.data));
-  }, [parkingZone?.id]);
-  const onGetFreeSlot = (parkingZoneId) => {
-    parkingZoneService.getBookedSlot(parkingZoneId).then(res => setFreeSlots(parkingZone.slots - res.data));
-  };
-  const getDetailDescription = () => {
-    if (!parkingZone) return [];
-    return [
-      {
-        key: 1,
-        label: 'Địa chỉ',
-        children: parkingZone.detailAddress,
-      },
-      {
-        key: 2,
-        label: 'Xã/Phường',
-        children: parkingZone.commune.name,
-      },
-      {
-        key: 3,
-        label: 'Quận/Huyện',
-        children: parkingZone.commune.district.name,
-      },
-      {
-        key: 4,
-        label: 'Tỉnh/Thành phố',
-        children: parkingZone.commune.district.city.name,
-      },
-      {
-        key: 5,
-        label: 'Giá thành mỗi giờ (VNĐ)',
-        children: parkingZone.pricePerHour ?? 0,
-      },
-      {
-        key: 6,
-        label: 'Giá khi đỗ quá giờ (VNĐ)',
-        children: parkingZone.priceOverTimePerHour ?? 0,
-      },
-      {
-        key: 5,
-        label: 'Số chỗ trống',
-        children: (
-          <Button
-            onClick={() => onGetFreeSlot(parkingZone.id)}
-          >{freeSlots ?? parkingZone.slots}</Button>
-        ),
-      },
+            key: '1',
+            label: 'Chi tiết',
+            children: (<Descriptions
+                bordered
+                items={getDetailDescription()}
+                size='small'
+                column={{xs: 1, sm: 1, md: 1, lg: 1, xl: 2, xxl: 2}}
+            />),
+        },
+        {
+            key: '2',
+            label: 'Xem đánh giá',
+            children: <FeedbackList parkingZoneId={parkingZone?.id}/>,
+        },
+        {
+            key: '3',
+            label: 'Đặt chỗ',
+            children: (<BookingForm
+                parkingZone={parkingZone}
+
+            ></BookingForm>),
+        },
+        {
+            key: '4',
+            label: 'Viết đánh giá',
+            children: (<FeedBackForm
+                parkingZoneId={parkingZone?.id}
+            />),
+        },
     ];
-  };
-  const items = [
-    {
+    const onChangeTabs = (key) => {
+        setTab(key);
+    };
 
-      key: '1',
-      label: 'Chi tiết',
-      children: (<Descriptions
-        bordered
-        items={getDetailDescription()}
-        size='small'
-        column={{ xs: 1, sm: 1, md: 1, lg: 1, xl: 2, xxl: 2 }}
-      />),
-    },
-    {
-      key: '2',
-      label: 'Xem đánh giá',
-      children: <FeedbackList parkingZoneId={parkingZone?.id} />,
-    },
-    {
-      key: '3',
-      label: 'Đặt chỗ',
-      children: (<BookingForm
-        parkingZone={parkingZone}
-
-      ></BookingForm>),
-    },
-    {
-      key: '4',
-      label: 'Viết đánh giá',
-      children: (<FeedBackForm
-        parkingZoneId={parkingZone?.id}
-      />),
-    },
-  ];
-  const onChangeTabs = (key) => {
-    setTab(key);
-  };
-
-  const arrayBookmark = getListBookmarkParkingZone()??[];
-  const [isBookmark, setIsBookmark] = useState(isShow&&arrayBookmark.includes(parkingZone.id));
-
-  const ClickBookMark = () => {
-    let arrayBookmarkPzId = getListBookmarkParkingZone()??[];
-    if(!isBookmark){
-      // eslint-disable-next-line react/prop-types
-      arrayBookmarkPzId.push(parkingZone.id);
-      setIsBookmark(true);
-    }else {
-      // eslint-disable-next-line react/prop-types
-      arrayBookmarkPzId = arrayBookmarkPzId.filter(item => item !== parkingZone.id);
-      setIsBookmark(false);
+    const [isBookmark, setIsBookmark] = useState(false);
+    const ClickBookMark = () => {
+        let arrayBookmarkPzId = getListBookmarkParkingZone() ?? [];
+        if (!isBookmark) {
+            // eslint-disable-next-line react/prop-types
+            arrayBookmarkPzId.push(parkingZone.id);
+            setIsBookmark(true);
+            notification.success({
+                message: 'Lưu thành công',
+            });
+        } else {
+            // eslint-disable-next-line react/prop-types
+            arrayBookmarkPzId = arrayBookmarkPzId.filter(item => item !== parkingZone.id);
+            setIsBookmark(false);
+            notification.success({
+                message: 'Bỏ lưu thành công',
+            });
+        }
+        setListBookmarkParkingZone(arrayBookmarkPzId);
     }
-    setListBookmarkParkingZone(arrayBookmarkPzId);
-  }
-  const operations = <Button icon={isBookmark?<BookTwoTone/>:<BookOutlined/>} onClick={ClickBookMark}></Button>;
+    // const textTooltipBookmark = <span>Lưu bãi đỗ xe</span>;
+    const operations = (
+        <>
+            <Tooltip placement="bottom" title={isBookmark ? 'Bỏ lưu bãi đỗ xe' : 'Lưu bãi đỗ xe'} zIndex={10000}>
+                <Button icon={isBookmark ? <BookTwoTone/> : <BookOutlined/>} onClick={ClickBookMark}></Button>
+            </Tooltip>
+        </>);
 
-  return (
-    <>
-      <Modal
-        open={isShow}
-        onCancel={onCloseCallback}
-        zIndex={2000}
-        okButtonProps={{
-          style: {
-            display: 'none',
-          },
-        }}
-        cancelButtonProps={{
-          style: {
-            display: 'none',
-          },
-        }}
-        closable={true}
-        title={parkingZone?.name}
-      >
-        <Image.PreviewGroup>
-          <Carousel autoplay dotPosition='top' style={{}}>
-            {imageLinks.map((val, index) => {
-              return <Image key={index} src={val}></Image>;
-            })}
-          </Carousel>
-        </Image.PreviewGroup>
-        <div className={('pt-[10px]')}>
-          <Tabs
-            activeKey={tab}
-            items={items}
-            destroyInactiveTabPane={true}
-            onChange={onChangeTabs}
-            tabBarExtraContent={operations}/>
-        </div>
-      </Modal>
-    </>
-  );
+    return (
+        <>
+            <Modal
+                open={isShow}
+                onCancel={onCloseCallback}
+                zIndex={2000}
+                okButtonProps={{
+                    style: {
+                        display: 'none',
+                    },
+                }}
+                cancelButtonProps={{
+                    style: {
+                        display: 'none',
+                    },
+                }}
+                closable={true}
+                title={parkingZone?.name}
+            >
+                <Image.PreviewGroup>
+                    <Carousel autoplay dotPosition='top' style={{}}>
+                        {imageLinks.map((val, index) => {
+                            return <Image key={index} src={val}></Image>;
+                        })}
+                    </Carousel>
+                </Image.PreviewGroup>
+                <div className={('pt-[10px]')}>
+                    <Tabs
+                        activeKey={tab}
+                        items={items}
+                        destroyInactiveTabPane={true}
+                        onChange={onChangeTabs}
+                        tabBarExtraContent={operations}/>
+                </div>
+            </Modal>
+        </>
+    );
 };
 export default ParkingZoneDetail;
