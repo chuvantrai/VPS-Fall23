caabb6128c040c34c9bf2c6a9e477495f05409b7
nhiep <nhiep2371@gmail.com>
2023-11-26 13:50:47 +0700

    add config soft delete

---
+++ VPS-Fall23/Service.ManagerVPS/Extensions/DbContext/SoftDeleteInterceptor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Service.ManagerVPS.Extensions.DbContext
+{
+    public class SoftDeleteInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            if (eventData.Context is null) return result;
+            foreach (var entry in eventData.Context.ChangeTracker.Entries())
+            {
+                if (entry is not { State: Microsoft.EntityFrameworkCore.EntityState.Deleted, Entity: ISoftDeleteEntity deleteEntity }) continue;
+                entry.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                deleteEntity.IsDeleted = true;
+
+            }
+            return result;
+        }
+    }
+}
