fbc1ecf65c239d7d522105ef895847e819368f10
nhiep <nhiep2371@gmail.com>
2023-11-26 14:01:47 +0700

    add saving change async interceptor

--- VPS-Fall23/Service.ManagerVPS/Extensions/DbContext/SoftDeleteInterceptor.cs
+++ VPS-Fall23/Service.ManagerVPS/Extensions/DbContext/SoftDeleteInterceptor.cs
@@ -16,5 +16,17 @@ namespace Service.ManagerVPS.Extensions.DbContext
             }
             return result;
         }
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            if (eventData.Context is null) return ValueTask.FromResult(result);
+            foreach (var entry in eventData.Context.ChangeTracker.Entries())
+            {
+                if (entry is not { State: Microsoft.EntityFrameworkCore.EntityState.Deleted, Entity: ISoftDeleteEntity deleteEntity }) continue;
+                entry.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                deleteEntity.IsDeleted = true;
+
+            }
+            return ValueTask.FromResult(result);
+        }
     }
 }
